83bd2421713016cce05347b20881059d721c3ff7
frank0434 <jian.liu@plantandfood.co.nz>
2019-10-08 11:31:54 +1300

    files not want to show

--- credentials.R
+++
@@ -1,7 +0,0 @@
-#the credential for an application database 
-
-Rainshelter_SWC <- list(dbname = "cflfcl_Rainshelter_SWC",
-                         host = "database.powerplant.pfr.co.nz",
-                         username = "cflfcl_Rainshelter_SWC",
-                         password = "o654UkI6iGNwhzHu")
-
